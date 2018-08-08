2c6df936dbadff4995a76f99e3e045882b6f968f
Harun  Hadzic <>
2018-08-08 15:55:08 -0400

    initial

---
+++ emoji class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoji class
+//
+//  Created by Harun  Hadzic on 8/8/18.
+//  Copyright © 2018 Harun  Hadzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
