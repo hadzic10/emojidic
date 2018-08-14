47452ce9894ea51d7a2f49f443677ba5f0ccdcbf
Harun  Hadzic <>
2018-08-14 18:08:23 -0400

    finished

--- emoji class/DefinitionViewController.swift
+++ emoji class/DefinitionViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
 
+    
+    @IBOutlet weak var DefinitionLabel: UILabel!
+    
     @IBOutlet weak var emojiLabel: UILabel!
     var emoji = "NO EMOJI"
     
@@ -19,8 +22,37 @@ class DefinitionViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         emojiLabel.text = emoji
-
         
+        if emoji == "🤢" {
+            DefinitionLabel.text = "Sick mans"
+        }
+        if emoji == "😎" {
+            DefinitionLabel.text = "He's got some shades"
+        }
+        if emoji == "😘" {
+            DefinitionLabel.text = "Blowing Kiss"
+        }
+        if emoji == "🙂" {
+            DefinitionLabel.text = "Smile Face"
+        }
+        if emoji == "🤥" {
+            DefinitionLabel.text = "He lies"
+        }
+        if emoji == "🤩" {
+            DefinitionLabel.text = "Stars"
+        }
+        if emoji == "💩" {
+            DefinitionLabel.text = "Savy"
+        }
+        if emoji == "👳🏻‍♂️" {
+            DefinitionLabel.text = "Towel Head"
+        }
+        if emoji == "⛄️" {
+            DefinitionLabel.text = "Snowman"
+        }
+        if emoji == "🤓" {
+            DefinitionLabel.text = "Nerdy Smile"
+        }
     }
 
     override func didReceiveMemoryWarning() {
